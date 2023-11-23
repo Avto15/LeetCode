ea5cf414aee297f91afa7a250e0acf178f97aa68
avtoa <Ananidze.Avtandil@kiu.edu.ge>
2023-11-23 16:07:02 +0400

    forLeetcode

---
+++ src/palindromeNumber/main.java
@@ -0,0 +1,22 @@
+package palindromeNumber;
+
+public class main {
+    public static void main(String[] args) {;
+    }
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+}
---
+++ src/palindromeNumber/solution.java
@@ -0,0 +1,4 @@
+package palindromeNumber;
+
+public class solution {
+}
